bf3517f16a3505e6399a354edc8ff4d19d9ff548
Santiago Junco <sjunco@prexcard.com>
2023-09-11 00:18:47 -0300

    Agregar configuracion de base de datos

---
+++ src/tpfinal/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+
+# escribime la configuracion de la base de datos sqlite
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'BlogDb',
+        'USER': 'postgres_user',
+        'PASSWORD': 'postgres_pass',
+        'HOST': 'db',
+        'PORT': '5432',
+    }
+}
