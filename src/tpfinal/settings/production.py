d77c58cd0a975c71a44789a66eadcb52c0866e80
Santiago Junco <sjunco@prexcard.com>
2023-10-15 01:56:25 -0300

    Cambiar base de datos

--- src/tpfinal/settings/production.py
+++ src/tpfinal/settings/production.py
@@ -7,13 +7,20 @@ ALLOWED_HOSTS = ['*']
 
 # escribime la configuracion de la base de datos sqlite
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'BlogDb',
+#         'USER': 'postgres_user',
+#         'PASSWORD': 'postgres_pass',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BlogDb',
-        'USER': 'postgres_user',
-        'PASSWORD': 'postgres_pass',
-        'HOST': 'db',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
